aa429871fcd8251fde5ded53dc25b29b4bff19b0
tereg <teregalla@gmail.com>
2016-12-29 15:02:46 -0800

    Add VideoDetail, VideoList, and VideoListItem components

--- src/index.js
+++ src/index.js
@@ -1,21 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyDYsgBnQogVOnp5gB_JMfCB4FQtaaQ1qqY';
 
-YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
-  console.log(data);
-});
-
 //Create a new component. This component should produce some HTML.
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = { 
+      videos: [],
+      selectedVideo: null
+    };
+    
+    YTSearch({key: API_KEY, term: 'italy'}, (videos) => {
+      this.setState({ 
+        videos: videos,
+        selectedVideo: videos[0]
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList 
+          //We defined a function that takes a video and defines it on app's state in line below:
+          onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
+          videos={this.state.videos} />
+      </div>
+    );
+  }
 }
 
 //Take this component’s generated HTML and put it on the page (in the DOM).
